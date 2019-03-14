a2e0aff17f53859209f1a1a210d092db525c1bd2
weko3-dev15 <weko3-hitachi-dev15@ml.itg.hitachi.co.jp>
2019-03-14 10:56:13 +0900

    utils.py modified

--- invenio_stats/utils.py
+++ invenio_stats/utils.py
@@ -112,7 +112,7 @@ def default_permission_factory(query_name, params):
         )
 
 
-def build_record_stats(bucket_key, file_key):
+def build_record_stats(bucket_id, file_key):
     """Build the record's stats."""
     stats = {}
     stats_sources = {
@@ -124,7 +124,7 @@ def build_record_stats(bucket_key, file_key):
     #         },
     #     },
         'bucket-file-download-total': {
-            'params': {'bucket_key': bucket_key, 'file_key': file_key},
+            'params': {'bucket_id': bucket_id, 'file_key': file_key},
             'fields': {
                 'downloads': 'count',
                 'unique_downloads': 'unique_count',
