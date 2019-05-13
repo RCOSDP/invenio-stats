9660bc401f12feb29bfa8b9d378ed9330a136bda
weko_dev17 <weko3-hitachi-dev17@ml.itg.hitachi.co.jp>
2019-05-13 16:51:06 +0700

    Item views report

--- invenio_stats/views.py
+++ invenio_stats/views.py
@@ -586,6 +586,6 @@ blueprint.add_url_rule(
 )
 
 blueprint.add_url_rule(
-    '/record_view/<int:year>/<int:month>',
+    '/report/record/record_view/<int:year>/<int:month>',
     view_func=record_view_report,
 )
