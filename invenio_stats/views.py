a28052567c83f231b1919f1fbe6be7b87788c1a1
weko3-dev22 <weko3-hitachi-dev22@ml.itg.hitachi.co.jp>
2019-05-23 14:03:11 +0700

    fix bug display report without select date

--- invenio_stats/views.py
+++ invenio_stats/views.py
@@ -429,7 +429,7 @@ class QueryItemRegReport(ContentNegotiatedMethodView):
                 query_name = 'item-detail-item-total'
             else:
                 query_name = 'item-detail-total' if not empty_date_flg \
-                    else 'bucket-item-detail-view-histogram'
+                    and unit != 'Host' else 'bucket-item-detail-view-histogram'
 
         # total
         query_total_cfg = current_stats.queries[query_name]
@@ -534,13 +534,15 @@ class QueryItemRegReport(ContentNegotiatedMethodView):
                         })
 
             elif unit == 'Host':
-                result = []
-                start_date_string = start_date.strftime('%Y-%m-%d')
-                end_date_string = end_date.strftime('%Y-%m-%d')
-                params = {
-                          'start_date': start_date_string,
-                          'end_date': end_date_string
-                          }
+                start_date_string = ''
+                end_date_string = ''
+                params = {}
+                if start_date is not None:
+                    start_date_string = start_date.strftime('%Y-%m-%d')
+                    params.update({'start_date': start_date_string})
+                if end_date is not None:
+                    end_date_string = end_date.strftime('%Y-%m-%d')
+                    params.update({'end_date': end_date_string})
                 res_total = query_total.run(**params)
                 for item in res_total['buckets']:
                     for h in item['buckets']:
